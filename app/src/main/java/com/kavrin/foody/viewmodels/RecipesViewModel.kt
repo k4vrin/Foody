14e88dc9c00ff628640554e05ab14c81ab4d1569
Mostafa Hosseini <kavrindev@gmail.com>
2022-05-09 00:23:44 +0430

    Add Preferences DataStore and complete BottomSheet functionality

--- app/src/main/java/com/kavrin/foody/viewmodels/RecipesViewModel.kt
+++ app/src/main/java/com/kavrin/foody/viewmodels/RecipesViewModel.kt
@@ -1,7 +1,13 @@
 package com.kavrin.foody.viewmodels
 
 import android.app.Application
+import android.util.Log
 import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.viewModelScope
+import com.kavrin.foody.data.DataStoreRepository
+import com.kavrin.foody.util.Constants.DEFAULT_DIET_TYPE
+import com.kavrin.foody.util.Constants.DEFAULT_MEAL_TYPE
+import com.kavrin.foody.util.Constants.DEFAULT_RECIPES_NUMBER
 import com.kavrin.foody.util.Constants.QUERY_API_KEY
 import com.kavrin.foody.util.Constants.QUERY_DIET
 import com.kavrin.foody.util.Constants.QUERY_INGREDIENTS
@@ -9,21 +15,59 @@ import com.kavrin.foody.util.Constants.QUERY_NUMBER
 import com.kavrin.foody.util.Constants.QUERY_RECIPE
 import com.kavrin.foody.util.Constants.QUERY_TYPE
 import com.kavrin.foody.util.PrConstants.API_KEY
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import javax.inject.Inject
 
-class RecipesViewModel(application: Application) : AndroidViewModel(application) {
+@HiltViewModel
+class RecipesViewModel @Inject constructor(
+    private val dataStoreRepository: DataStoreRepository,
+    application: Application
+) : AndroidViewModel(application) {
+
+    private var mealType = DEFAULT_MEAL_TYPE
+    private var dietType = DEFAULT_DIET_TYPE
+
+    val readMealAndDietType = dataStoreRepository.readMealAndDietType
+
 
     fun applyQueries(): HashMap<String, String> {
         val queries: HashMap<String, String> = HashMap()
+        // Collect the values from DataStore and pass it to the query
+        viewModelScope.launch {
+            readMealAndDietType.collect {
+                mealType = it.selectedMealType // If there is no value we pass our Default values
+                dietType = it.selectedDietType // that we specified in DataStoreRepository
+            }
+        }
 
-        queries[QUERY_NUMBER] = "50"
+        queries[QUERY_NUMBER] = DEFAULT_RECIPES_NUMBER
         queries[QUERY_API_KEY] = API_KEY
-        queries[QUERY_TYPE] = "main course"
-        queries[QUERY_DIET] = "gluten free"
+        queries[QUERY_TYPE] = mealType
+        queries[QUERY_DIET] = dietType
         queries[QUERY_RECIPE] = "true"
         queries[QUERY_INGREDIENTS] = "true"
-
+        Log.d("RecipesBottomSheet", "queries: $queries")
         return queries
     }
 
-
+    /**
+     * Save meal and diet type
+     *
+     * @param mealType
+     * @param mealTypeId
+     * @param dietType
+     * @param dietTypeId
+     *
+     * Insert values to the DataStore
+     */
+    fun saveMealAndDietType(
+        mealType: String,
+        mealTypeId: Int,
+        dietType: String,
+        dietTypeId: Int
+    ) = viewModelScope.launch(Dispatchers.IO) {
+        dataStoreRepository.saveMealAndDietType(mealType, mealTypeId, dietType, dietTypeId)
+    }
 }
