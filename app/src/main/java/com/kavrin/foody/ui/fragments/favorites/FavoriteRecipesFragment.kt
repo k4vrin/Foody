e2d8e2288e4383ce2257c1415b7a5487dc1d1f5f
Mostafa Hosseini <kavrindev@gmail.com>
2022-05-16 16:25:09 +0430

    Add FavoriteRecipesFragment Menu

--- app/src/main/java/com/kavrin/foody/ui/fragments/favorites/FavoriteRecipesFragment.kt
+++ app/src/main/java/com/kavrin/foody/ui/fragments/favorites/FavoriteRecipesFragment.kt
@@ -1,13 +1,13 @@
 package com.kavrin.foody.ui.fragments.favorites
 
 import android.os.Bundle
+import android.view.*
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.lifecycleScope
 import androidx.recyclerview.widget.LinearLayoutManager
+import com.google.android.material.snackbar.Snackbar
+import com.kavrin.foody.R
 import com.kavrin.foody.adapters.FavoritesAdapter
 import com.kavrin.foody.databinding.FragmentFavoriteRecipesBinding
 import com.kavrin.foody.viewmodels.MainViewModel
@@ -35,20 +35,42 @@ class FavoriteRecipesFragment : Fragment() {
         binding.mAdapter = mAdapter
 
         setupRecyclerView()
-
-//        lifecycleScope.launch {
-//            mMainViewModel.readFavoriteRecipes.observe(viewLifecycleOwner) { favorites ->
-//                mAdapter.setData(newData = favorites)
-//            }
-//        }
+        // Initialize option menu
+        setHasOptionsMenu(true)
 
         return binding.root
     }
 
+    /**
+     * Setup recycler view
+     *
+     */
     private fun setupRecyclerView() {
         val recyclerView = binding.favoriteRecyclerView
         recyclerView.adapter = mAdapter
         recyclerView.layoutManager = LinearLayoutManager(requireContext())
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        inflater.inflate(R.menu.favorite_recipes_menu, menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        // Delete all
+        if (item.itemId == R.id.deleteAll_favorite_recipes_menu) {
+            mMainViewModel.deleteAllFavoriteRecipes()
+            showSnackBar("All recipes has been removed")
+            }
+        return super.onOptionsItemSelected(item)
+    }
+
+    private fun showSnackBar(message: String) {
+        Snackbar.make(
+            binding.root,
+            message,
+            Snackbar.LENGTH_SHORT
+        ).setAction("Okay"){}
+            .show()
     }
 
     override fun onDestroyView() {
